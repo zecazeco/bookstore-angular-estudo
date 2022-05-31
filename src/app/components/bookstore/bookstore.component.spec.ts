6db1399938ca669d3bebd9878a9002ee5df8363d
zecazeco <9203689+zecazeco@users.noreply.github.com>
2022-05-31 09:49:32 -0300

    first commit

---
+++ src/app/components/bookstore/bookstore.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookstoreComponent } from './bookstore.component';
+
+describe('BookstoreComponent', () => {
+  let component: BookstoreComponent;
+  let fixture: ComponentFixture<BookstoreComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BookstoreComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookstoreComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
